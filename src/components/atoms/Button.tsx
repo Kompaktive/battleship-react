d0f73b40bf1dcbdb22fa1e97b23d11db2adc1d7b
Kompaktive <alfian.kurniadi.dev@gmail.com>
2024-02-05 15:02:22 +0700

    feat(button): add color transition on hover

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -17,7 +17,7 @@ const Button = ({
   return (
     <button
       {...props}
-      className={`${!!className ? `${className} ` : ""}text-accent select-none text-4xl font-medium hover:text-primary`}
+      className={`${!!className ? `${className} ` : ""}text-accent select-none text-4xl font-medium transition duration-100 hover:text-primary`}
     >
       {children}
     </button>
